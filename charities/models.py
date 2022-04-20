d12d25ae118d5c6edba3abe7551e355b029ddf5b
Elise La Rooy <eliselarooy@gmail.com>
2022-04-20 18:40:01 +0100

    Created charities seed file

--- charities/models.py
+++ charities/models.py
@@ -2,7 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+  name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+
 
 class Charity(models.Model):
-    name = models.CharField(max_length=100)
-    logo = models.CharField(max_length=100, blank=True)
+    name = models.CharField(max_length=200)
+    logo = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    category = models.ManyToManyField(Category, related_name='charity', blank=True)
+
+    def __str__(self):
+      return self.name
